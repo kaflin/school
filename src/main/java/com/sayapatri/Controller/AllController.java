adee7b8556e4d92c48ce3dae9f1daa5ef341d93b
kaflin <suraj520876@gmail.com>
2025-07-10 19:07:57 +0545

    updated AllController Code

--- src/main/java/com/sayapatri/Controller/AllController.java
+++ src/main/java/com/sayapatri/Controller/AllController.java
@@ -17,106 +17,94 @@ public class AllController {
     private NoticeService noticeService;
 
     @GetMapping("/principalDesk")
-    public String showPrincipal(Notice notice) {
+    public String showPrincipal() {
         return "principal";
     }
 
-   @GetMapping("/faculty")
-    public String showFaculty(Notice notice) {
-
-        return "faculty";
-    }
+    @GetMapping("/faculty")
+    public String showFaculty() {return "faculty";}
 
     @GetMapping("/admin")
-    public String showAdmin(Notice notice) {
+    public String showAdmin() {
         return "admission";
     }
 
     @GetMapping("/infra")
-    public String showInfra(Notice notice) {
+    public String showInfra() {
         return "infrastructures";
     }
 
     @GetMapping("/affil")
-    public String showAffiliation(Notice notice) {
+    public String showAffiliation() {
         return "affiliation";
     }
 
     @GetMapping("/home")
     public String showHome(Model model) {
-        model.addAttribute("notices",noticeService.findAll());
+        model.addAttribute("notices", noticeService.findAll());
         return "index";
     }
+
     @GetMapping("/houseSystem")
-    public String showHouseSystem(Notice notice) {
+    public String showHouseSystem() {
         return "HouseSystem";
     }
+
     @GetMapping("/schoolTransport")
-    public String showSchoolTransport(Notice notice)
-    {
+    public String showSchoolTransport() {
         return "SchoolTransport";
     }
 
     @GetMapping("/attendance")
-    public String showAttendance(Notice notice) {
+    public String showAttendance() {
         return "Attendance";
     }
 
     @GetMapping("/sports")
-    public String showSports(Notice notice) {
+    public String showSports() {
         return "Sports";
     }
 
 
     @GetMapping("/project")
-    public String showProjectWork(Notice notice)
-
-    {
+    public String showProjectWork() {
         return "ProjectWork";
     }
 
     @GetMapping("/curriculum")
-    public String showCurriculum(Notice notice) {
+    public String showCurriculum() {
         return "Curriculum";
     }
 
-    @GetMapping("/studentcouncil")
-    public String showStudentCouncil(Notice notice) {
+    @GetMapping("/studentCouncil")
+    public String showStudentCouncil() {
         return "StudentCouncil";
     }
 
     @GetMapping("/cocurricular")
-    public String showCoCurricular(Notice notice) {
+    public String showCoCurricular() {
         return "cocurricular";
     }
 
     @GetMapping("/gallery")
-    public String showGallery(Notice notice) {
+    public String showGallery() {
         return "Gallery";
     }
 
 
     @GetMapping("/readMore")
-    public String showReadMore(Notice notice)
-
-    {
+    public String showReadMore() {
         return "ReadMore";
     }
 
-
-
-
     @GetMapping("/fullNotice/{id}")
     public String showFullNotice(@PathVariable("id") long id, Model model) {
-
-        Notice notice=noticeRepository.findById(id)
-        .orElseThrow(()->new IllegalArgumentException("Invalid notice Id:"+id));
+        Notice notice = noticeRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid notice Id:" + id));
         System.out.println(noticeRepository.findById(id));
-
-        model.addAttribute("notices",notice);
+        model.addAttribute("notices", notice);
         return "fullNotice";
     }
-
 
 
 }
